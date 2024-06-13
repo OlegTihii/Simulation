a5c43893a369010901d6507671cf7f9d970ec061
OlegTihii <YouDoontKnow>
2024-06-13 13:49:19 +0300

    feature: create map for simulation

--- src/main/java/org/petproject/Main.java
+++ src/main/java/org/petproject/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static Simulation simulation = new Simulation();
 
     public static void main(String[] args) {
-
+        simulation.startSimulation();
     }
 }
---
+++ src/main/java/org/petproject/entity/staticEntity/Ground.java
@@ -0,0 +1,11 @@
+package org.petproject.entity.staticEntity;
+
+import org.petproject.entity.Entity;
+
+public class Ground extends Entity {
+
+    @Override
+    public String toString() {
+        return "🟫";
+    }
+}
