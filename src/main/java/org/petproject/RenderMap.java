0401a7a3f0cc69ddc5d0f9d07f541350089e8f01
OlegTihii <YouDoontKnow>
2024-06-17 13:48:32 +0300

    refactor: add new Entity.

--- src/main/java/org/petproject/RenderMap.java
+++ src/main/java/org/petproject/RenderMap.java
@@ -1,9 +1,14 @@
 package org.petproject;
 
 import org.petproject.entity.Entity;
+import org.petproject.entity.creature.Herbivore;
+import org.petproject.entity.creature.Hunter;
+import org.petproject.entity.staticEntity.Grass;
 import org.petproject.entity.staticEntity.Ground;
 
-import java.util.*;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class RenderMap {
@@ -13,13 +18,15 @@ public class RenderMap {
 
     public Map<Coordinate, Entity> map = new TreeMap<>();
 
-
     public void addEntityOnMap() {
         for (int i = 0; i < RenderMap.LIMIT_X; i++) {
             for (int j = 0; j < RenderMap.LIMIT_Y; j++) {
                 map.put(new Coordinate(i, j), new Ground());
             }
         }
+        map.put(new Coordinate(new Random().nextInt(20), new Random().nextInt(20)), new Herbivore());
+        map.put(new Coordinate(new Random().nextInt(20), new Random().nextInt(20)), new Hunter());
+        map.put(new Coordinate(new Random().nextInt(20), new Random().nextInt(20)), new Grass());
     }
 
     public void printMap() {
