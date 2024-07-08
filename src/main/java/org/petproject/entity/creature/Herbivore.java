115d579f9042cd2117479a7db6f367d063ba79bd
OlegTihii <YouDoontKnow>
2024-07-08 17:20:19 +0300

    refactor: -class Herbivore: Refresh eat()-> eatGrass(); -class Hunter: Refresh eat()-> attackHerbivore();

--- src/main/java/org/petproject/entity/creature/Herbivore.java
+++ src/main/java/org/petproject/entity/creature/Herbivore.java
@@ -2,14 +2,14 @@ package org.petproject.entity.creature;
 
 public class Herbivore extends Creature {
 
+    public void eatGrass() {
+
+    }
+
     @Override
     public void makeMove() {
     }
 
-    @Override
-    public void eat() {
-
-    }
 
     @Override
     public String toString() {
